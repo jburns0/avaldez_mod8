8d16066735f5aaf9d9583ab8bdc192200601df05
Allison Valdez <ajsnipes@usfca.edu>
2022-03-20 13:04:07 -0700

    stopping point: TypeError: string indices must be integers

--- lfsr.py
+++ lfsr.py
@@ -1,24 +1,28 @@
 class LFSR:
     # create an LFSR with initial state 'seed' and tap 'tap'
-    def __init__(self, initial_seed="", tap=1 ):
+    def __init__(self, initial_seed="", tap=1):
         self.initial_seed = initial_seed
         self.tap = tap
 
     # return the bit at index 'i'
-    def bit(self, i = int):
-        return self.seed[0]
+    def bit(self, i=int):
+        print(type(self.bit))
+        return int(self.initial_seed[0])
 
     # execute one LFSR iteration and return new (rightmost) bit as an int
     # you will find the binary XOR operator useful here
     def step(self):
-       new_seed = self.initial_seed << 1
-       right_bit = self.bit() ^ self.initial_seed[tap]
-       new_seed = new_seed + right_bit
-       return new_seed
+        new_seed = int(self.initial_seed) << 1
+        print(type({self.bit}))
+        right_bit = int(self.bit()) ^ self.initial_seed[tap]
+        new_seed = new_seed + right_bit
+        return new_seed
 
     # return string representation of the LFSR, ex: 01001010
     def __str__(self):
         return f"{self.new_seed}  {self.right_bit}"
+
+
 # your executable code that invokes LFSR
 if __name__ == '__main__':
     lfsr = LFSR()
